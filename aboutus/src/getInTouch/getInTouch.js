0902820ab908938a55ed85cd027e0b6bf79f6804
shahirzain <shahirzain100@gmail.com>
2019-02-21 16:49:35 +0500

    setup

---
+++ aboutus/src/getInTouch/getInTouch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class getInTouch extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default getInTouch
---
+++ aboutus/src/ourService/ourService.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class ourService extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ourService
